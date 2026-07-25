923d5eaa0e788166ec176bdfae742d0e61c914b4
ThomasN1035 <norwood172@usf.edu>
2026-07-24 22:40:20 -0400

    Configure Docker provider and resources in main.tf
    Add Docker provider and resources for image and container

--- main.tf
+++ main.tf
@@ -1,1 +1,29 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 3.0.0"
+    }
+  }
+}
 
+provider "docker" {
+  host = "unix:///var/run/docker.sock"
+}
+
+# Reference the image built by Jenkins
+resource "docker_image" "app_image" {
+  name         = "my-local-app:latest"
+  keep_locally = true
+}
+
+# Start the application container
+resource "docker_container" "app_container" {
+  image = docker_image.app_image.image_id
+  name  = "production-app"
+  
+  ports {
+    internal = 80
+    external = 8081
+  }
+}
